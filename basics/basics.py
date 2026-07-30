444452496f0ab21915449db070e64f3e35c039d9
Han Kwan Saing <saihankwansaing@gmail.com>
2026-07-30 15:09:45 +0900

    learned variables and type casting

--- basics/basics.py
+++ basics/basics.py
@@ -1,1 +1,11 @@
-print("hello world")
+name = "sai han"
+age = 27
+email = "saihan@example.com"
+is_student = True
+
+age = str(age)
+
+# print("hello world")
+# print(f"your name {name} and you're {age}. contact info = {email}. are you a student: {is_student}")
+print(age)
+print(type(age))
