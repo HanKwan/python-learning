abe5aa4c001376d3e1f0219fb124e1735d6622b7
Han Kwan Saing <saihankwansaing@gmail.com>
2026-07-30 15:17:11 +0900

    learned user's input

--- basics/basics.py
+++ basics/basics.py
@@ -1,11 +1,14 @@
-name = "sai han"
-age = 27
-email = "saihan@example.com"
-is_student = True
+# name = "sai han"
+# age = 27
+# email = "saihan@example.com"
+# is_student = True
 
-age = str(age)
+# age = str(age)
 
 # print("hello world")
 # print(f"your name {name} and you're {age}. contact info = {email}. are you a student: {is_student}")
-print(age)
-print(type(age))
+# print(age)
+# print(type(age))
+
+name = input("Enter your name: ")
+print(f"Hello {name}")
