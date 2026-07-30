d1c650c84c1f5421db33f35d6535aebb1e0051b2
Han Kwan Saing <saihankwansaing@gmail.com>
2026-07-30 15:38:03 +0900

    learned simple if statement with user input to create simple shopping cart

--- basics/basics.py
+++ basics/basics.py
@@ -10,5 +10,29 @@
 # print(age)
 # print(type(age))
 
-name = input("Enter your name: ")
-print(f"Hello {name}")
+# name = input("Enter your name: ")
+# print(f"Hello {name}")
+
+# shopping cart
+apple_price = 500
+pizza_price = 1200
+ramen_price = 1300
+
+price = 0
+
+item = input("What item would you like to buy?: ")
+
+if item == "apple":
+    price += apple_price
+elif item == "pizza":
+    price += pizza_price
+elif item == "ramen":
+    price += ramen_price
+else:
+    print(f"{item} is not on the menu")
+    exit()
+
+quantity = int(input("How many do you want to buy?: "))
+total_price = price * quantity
+
+print(f"Your total amount will be {total_price} yen")
